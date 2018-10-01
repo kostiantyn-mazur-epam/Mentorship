8a35766a6cc768329750e666e6e692ff0e8d1b0c
Kostiantyn Mazur <Kostiantyn_Mazur@epam.com>
2018-10-01 13:22:37 +0300

    Added Zoo GC task

---
+++ GC/ZooInDanger/ZooInDanger/Animals/Cat.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Zoo.Animals
+{
+    public class Cat : Animal
+    {
+        public Cat(IAnimalStatusTracker statusTracker) : base(statusTracker)
+        {
+        }
+
+        public override int LifeInterval
+        {
+            get { return 13; }
+        }
+
+        public override int InfectionDeathInterval
+        {
+            get { return 300; }
+        }
+
+        ~Cat()
+        {
+            Logger.LogYellow("Finalizing cat!");
+
+            // Release the object only in case number of corpses > 100
+        }
+    }
+}
