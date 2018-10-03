551fb601b7885123d3181a33a60863aba59c13a7
Kostiantyn Mazur <Kostiantyn_Mazur@epam.com>
2018-10-03 13:05:35 +0300

    Added task for resurrection

---
+++ GC/NoStaticResurrection/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace NoStaticResurrection
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Creating...");
+
+            var subject = new Zombie("Alexandro");
+
+            for (var i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Die already !");
+
+                GC.Collect();
+                GC.WaitForPendingFinalizers();
+            }
+        }
+    }
+}
