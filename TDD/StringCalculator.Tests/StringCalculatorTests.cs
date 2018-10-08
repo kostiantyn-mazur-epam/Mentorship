e512569185d9474fd76cc628b29909117fc683dd
Kostiantyn Mazur <Kostiantyn_Mazur@epam.com>
2018-10-08 12:35:58 +0300

    Added FizzBuzz and StringSum katas

--- TDD/StringCalculator.Tests/StringCalculatorTests.cs
+++ TDD/StringCalculator.Tests/StringCalculatorTests.cs
@@ -4,7 +4,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace StringCalculator.Tests
 {
     [TestClass]
-    public class UnitTest1
+    public class StringCalculatorTests
     {
         [TestMethod]
         public void Add_EmptyString_ShouldReturnZero()
