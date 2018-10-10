4c8c381f10373c01821f566a6b30468e02cfaedc
Kostiantyn Mazur <Kostiantyn_Mazur@epam.com>
2018-10-10 11:10:45 +0300

    Added console event task

---
+++ Patterns/ConsoleEvent/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleEvent
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Time to write something to console");
+
+            var listener = new ConsoleEventListener();
+            listener.Quit += ShowMessage;
+
+            for (var i = 0; i < 5; i++)
+            {
+                if (Console.ReadLine() == "quit")
+                {
+                    listener.Raise();
+                }
+            }
+        }
+
+        private static void ShowMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
