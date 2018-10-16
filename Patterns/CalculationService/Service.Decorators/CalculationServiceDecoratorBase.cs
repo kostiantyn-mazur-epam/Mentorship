d1ec1f7cbbb25600b5248542b7b056ed1ca26192
Kostiantyn Mazur <Kostiantyn_Mazur@epam.com>
2018-10-16 19:21:27 +0300

    Added calculation service task

---
+++ Patterns/CalculationService/Service.Decorators/CalculationServiceDecoratorBase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CalculationService.Service.Decorators
+{
+    internal abstract class CalculationServiceDecoratorBase : ICalculationService
+    {
+        protected ICalculationService _calculationService;
+
+        public CalculationServiceDecoratorBase(ICalculationService calculationService)
+        {
+            _calculationService = calculationService;
+        }
+
+        public abstract decimal Calculate(decimal firstParameter, decimal secondParameter);
+    }
+}
