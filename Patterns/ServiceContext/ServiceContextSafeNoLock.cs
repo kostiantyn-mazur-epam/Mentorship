ad9a0730df9fae07938c3cf0e5cc4f329945a488
Kostiantyn Mazur <Kostiantyn_Mazur@epam.com>
2018-10-19 15:39:53 +0300

    Added three singleton versions

---
+++ Patterns/ServiceContext/ServiceContextSafeNoLock.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading;
+
+namespace ServiceContext
+{
+    internal sealed class ServiceContextSafeNoLock
+    {
+        private static ServiceContextSafeNoLock _context;
+
+        private ServiceContextSafeNoLock()
+        {
+        }
+
+        public static ServiceContextSafeNoLock Instance
+        {
+            get
+            {
+                if (_context == null)
+                {
+                    var instance = new ServiceContextSafeNoLock();
+
+                    Interlocked.CompareExchange(ref _context, instance, null);
+                }
+
+                return _context;
+            }
+        }
+    }
+}
---
+++ Patterns/ServiceContext/ServiceContextUnsafe.cs
@@ -0,0 +1,24 @@
+﻿namespace ServiceContext.Unsafe
+{
+    internal sealed class ServiceContextUnsafe
+    {
+        private static ServiceContextUnsafe _context;
+
+        private ServiceContextUnsafe()
+        {
+        }
+
+        public static ServiceContextUnsafe Instance
+        {
+            get
+            {
+                if (_context == null)
+                {
+                    _context = new ServiceContextUnsafe();
+                }
+
+                return _context;
+            }
+        }
+    }
+}
