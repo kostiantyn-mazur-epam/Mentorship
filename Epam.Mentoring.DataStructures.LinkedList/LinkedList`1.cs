abf7f91adbd2f09fdeff3895316ac1d0c30abeb7
Kostiantyn Mazur <Kostiantyn_Mazur@epam.com>
2018-11-07 17:19:48 +0200

    Changed positioning algorithm

--- Epam.Mentoring.DataStructures.LinkedList/LinkedList`1.cs
+++ Epam.Mentoring.DataStructures.LinkedList/LinkedList`1.cs
@@ -10,11 +10,6 @@ namespace Epam.Mentoring.DataStructures
         private LinkedListNode<T> _head;
         private LinkedListNode<T> _tail;
 
-        public int Length
-        {
-            get => _size;
-        }
-
         public void Add(T item)
         {
             if (_size == 0)
@@ -49,13 +44,13 @@ namespace Epam.Mentoring.DataStructures
             }
             else
             {
-                if ((_size - position) >= (position + 2))
+                if (position <= _size / 2)
                 {
-                    InsertAt(item, ForwardTraversal(_head, position));
+                    InsertAt(item, BackwardTraversal(_tail, position, _size));
                 }
                 else
                 {
-                    InsertAt(item, BackwardTraversal(_tail, position, _size));
+                    InsertAt(item, ForwardTraversal(_head, position));
                 }
             }
         }
@@ -85,13 +80,45 @@ namespace Epam.Mentoring.DataStructures
             }
             else
             {
-                if ((_size - position) >= (position + 2))
+                if (position <= _size / 2)
                 {
                     DeleteNode(ForwardTraversal(_head, position));
                 }
                 else
                 {
                     DeleteNode(BackwardTraversal(_tail, position, _size));
+                }
+            }
+        }
+
+        public T ElementAt(int position)
+        {
+            if (_size == 0)
+            {
+                throw new InvalidOperationException("The list is empty");
+            }
+            if ((position < 0) || (position > (_size - 1)))
+            {
+                throw new ArgumentOutOfRangeException(nameof(position), position, "Incorrect position");
+            }
+
+            if (position == 0)
+            {
+                return _head.Item;
+            }
+            else if (position == (_size - 1))
+            {
+                return _tail.Item;
+            }
+            else
+            {
+                if (position <= _size / 2)
+                {
+                    return ForwardTraversal(_head, position).Item;
+                }
+                else
+                {
+                    return BackwardTraversal(_tail, position, _size).Item;
                 }
             }
         }
@@ -162,38 +189,6 @@ namespace Epam.Mentoring.DataStructures
             return node;
         }
 
-        public T ElementAt(int position)
-        {
-            if (_size == 0)
-            {
-                throw new InvalidOperationException("The list is empty");
-            }
-            if ((position < 0) || (position > (_size - 1)))
-            {
-                throw new ArgumentOutOfRangeException(nameof(position), position, "Incorrect position");
-            }
-
-            if (position == 0)
-            {
-                return _head.Item;
-            }
-            else if (position == (_size - 1))
-            {
-                return _tail.Item;
-            }
-            else
-            {
-                if ((_size - position) >= (position + 2))
-                {
-                    return ForwardTraversal(_head, position).Item;
-                }
-                else
-                {
-                    return BackwardTraversal(_tail, position, _size).Item;
-                }
-            }
-        }
-
         public IEnumerator<T> GetEnumerator()
         {
             return new LinkedListEnumerator<T>(_head);
@@ -202,6 +197,11 @@ namespace Epam.Mentoring.DataStructures
         IEnumerator IEnumerable.GetEnumerator()
         {
             return GetEnumerator();
+        }
+
+        public int Length
+        {
+            get => _size;
         }
     }
 }
